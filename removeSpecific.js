96b165bbf73ff8e3ab16a06aa4f538215935cadd
sonam_00 <sonam9tamang45@gmail.com>
2024-08-03 11:58:31 +0545

    specific element from an array removed

---
+++ removeSpecific.js
@@ -0,0 +1,5 @@
+//2.Write a program to remove a specific element from an array.
+
+const arr = ["bottlegourd", "bittergourd", "tomato", "brinjal"];
+const result = arr.splice(1, 1, "maseura");
+alert(`${arr}`);
